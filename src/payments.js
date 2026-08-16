c370feb5e43c5ca9b26ae8ac91fed7f2994239d3
Dillon Johnson <djrepec@gmail.com>
2026-08-16 16:13:42 -0400

    Add payments DB connection helper

---
+++ src/payments.js
@@ -0,0 +1,8 @@
+// Database connection for the payments service
+const DB_HOST = 'payments-db.internal.example.com'
+const DB_USER = 'payments_admin'
+const DB_PASSWORD = 'Pr0d-Payments-2026!' // TODO move to env
+
+export function connectionString() {
+  return `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/payments`
+}
