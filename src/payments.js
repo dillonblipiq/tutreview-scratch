efa9e99250c6d7db57800529ceb8f3f90da1e7db
Dillon Johnson <djrepec@gmail.com>
2026-08-16 23:12:47 -0400

    tweak A

--- src/payments.js
+++ src/payments.js
@@ -6,3 +6,4 @@ const DB_PASSWORD = 'Pr0d-Payments-2026!' // TODO move to env
 export function connectionString() {
   return `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/payments`
 }
+// touch A 1786936367
